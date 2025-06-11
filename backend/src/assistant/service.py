3c49c00ac22903ae84d76bd6070dc8caa09c66ae
muslik <ahmetaliev345@gmail.com>
2025-06-12 00:10:34 +0500

    [fix] : assistnat and docker

--- backend/src/assistant/service.py
+++ backend/src/assistant/service.py
@@ -1,45 +1,54 @@
 from autogen import AssistantAgent, GroupChat, GroupChatManager, UserProxyAgent
-import os
 from config import settings
+import os
 
-os.environ["OPENAI_API_KEY"] = settings
+os.environ["OPENAI_API_KEY"] = settings.OPENAI_API_KEY
 
 product_describer = AssistantAgent(
     name="ProductDescriber",
-    llm_config={"model": "gpt-4"}
+    llm_config={"model": "gpt-4"},
+    system_message="Ты создаешь красивое объявление о продаже продукта."
 )
 
-price_finder = AssistantAgent(
-    name="PriceFinder",
+lang_translarot = AssistantAgent(
+    name="LangTranslator",
     llm_config={"model": "gpt-4"},
+    system_message="Ты переводишь объявление на английский и казахский язык."
 )
 
-manager = AssistantAgent(
-    name="ProductManager",
+user = UserProxyAgent(
+    name="User",
+    human_input_mode="NEVER",
+    is_termination_msg=lambda msg: False  
+)
+
+groupchat = GroupChat(
+    agents=[user, product_describer, lang_translarot],
+    messages=[],
+    max_round=3
+)
+
+chat_manager = GroupChatManager(
+    groupchat=groupchat,
     llm_config={"model": "gpt-4"},
+    system_message="""
+Ты управляешь чатом. Когда пользователь пишет, первым отвечает ProductDescriber с красивым объявлением.
+Затем PriceFinder переводит объявление на английский и казахский. После этого можно завершить разговор.
+"""
 )
 
 def run_market_agents(user_input: str) -> str:
-    user = UserProxyAgent(
-        name="User",
-        human_input_mode="NEVER",
-        max_consecutive_auto_reply=10
-    )
-
-    groupchat = GroupChat(
-        agents=[user, product_describer, price_finder, manager],
-        messages=[],
-        max_round=8
-    )
-
-    manager_agent = GroupChatManager(
-        groupchat=groupchat,
-        llm_config={"model": "gpt-4"}
-    )
-
     user.initiate_chat(
-        manager_agent,
-        message=f"Хочу продать: {user_input}. Сгенерируй хорошее объявление и найди оптимальную цену."
+        chat_manager,
+        message=f"Хочу продать: {user_input}. Сгенерируй хорошее объявление"
     )
 
-    return groupchat.messages[-1]["content"]
+    messages = groupchat.messages
+
+    generated = [m["content"] for m in messages if m["name"] == "ProductDescriber"]
+    translated = [m["content"] for m in messages if m["name"] == "PriceFinder"]
+
+    return {
+        "generated": generated[-1] if generated else None,
+        "translated": translated[-1] if translated else None,
+    }
